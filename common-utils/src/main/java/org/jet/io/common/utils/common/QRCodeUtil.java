a364829af44b65731b54c4bf7af22271dcb9f1d0
Puffer <jetwong0115@gmail.com>
2021-05-14 12:20:04 +0800

    初次提交

--- common-utils/src/main/java/org/jet/io/common/utils/common/QRCodeUtil.java
+++ common-utils/src/main/java/org/jet/io/common/utils/common/QRCodeUtil.java
@@ -10,15 +10,22 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+
+import static com.google.zxing.client.j2se.MatrixToImageWriter.toBufferedImage;
 
 
-public class QRCodeUtil {
+public final class QRCodeUtil {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private QRCodeUtil() {
+    }
 
     /**
      * 生成格式为JPG的二维码工具
@@ -30,7 +37,7 @@ public class QRCodeUtil {
      * @throws WriterException
      * @throws IOException
      */
-    public void generateQRCodeFile(String content, int width, int height, Path path) throws WriterException, IOException {
+    public static void generateQRCodeFile(String content, int width, int height, Path path) throws WriterException, IOException {
         Map<EncodeHintType, Object> hints = new HashMap<>();
         hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
@@ -38,10 +45,15 @@ public class QRCodeUtil {
 
         BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, hints);
 
-        if (Objects.nonNull(bitMatrix)) {
-            MatrixToImageWriter.writeToPath(bitMatrix, "jpg", path);
-        } else {
-            log.debug("bitMatrix = NULL");
+        MatrixToImageWriter.writeToPath(bitMatrix, "jpg", path);
+
+    }
+
+    public static void writeToFile(BitMatrix matrix, String format, File file) throws IOException {
+        BufferedImage image = toBufferedImage(matrix);
+        if (!ImageIO.write(image, format, file)) {
+            throw new IOException("Could not write an image of format "
+                    + format + " to " + file);
         }
     }
 }
